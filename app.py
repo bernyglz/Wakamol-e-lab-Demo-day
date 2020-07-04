9a0cee77781e7125b57cd2438cca9e706aa158f5
Berny Glz <tato_09@hotmail.com>
2020-07-04 13:40:25 -0500

    modify app for heroku server address

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ y_Scaler_mty = joblib.load('y_scaler_mty.sav')
 @app.route('/')
 def home():
     "Rutas disponibles:"
-    "http://127.0.0.1:5000/predict/<room>/<bathroom>/<construction>/<terrain>/<direction>/<casa>/<casa_en_c>/<depto>/<nuevo>/<remate>"
-    "http://127.0.0.1:5000/datos"
+    "https://wakamol-e-lab-demo-day.herokuapp.com/predict/<room>/<bathroom>/<construction>/<terrain>/<direction>/<casa>/<casa_en_c>/<depto>/<nuevo>/<remate>"
+    "https://wakamol-e-lab-demo-day.herokuapp.com/datos"
     return render_template('index.html')
 
 #################################################
