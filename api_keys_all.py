af71bd575b6ee732e5c8155316e0040ea0cfb6cf
Berny Glz <tato_09@hotmail.com>
2020-07-06 19:42:47 -0500

    api key

--- api_keys_all.py
+++ api_keys_all.py
@@ -1,14 +1,6 @@
-#  Newyork Times API
-nyt_api_key = "gkMpDz1FCGWyEUcI9eb2blM19I2jYUk6"
-
-# OpenWeatherMap API Key
-weather_api_key = "572160991c261771a5b331a8ab8cc2bf"
 
 # Google API Key
 
-google_api_key = "AIzaSyACA9D2S1csNGa2-XXumdk_0PLvU6FCwx8"
+google_api_key = "AIzaSyA4ICMxv8kmKE7dKmD59epuqw2awGJ7SaE"
 #google_api_key = "AIzaSyDBcuTpPrgOxCXpwRyYkp17kaIPgzm9978"
 
-
-# Census US Data API Key
-census_api_key = "c277f95333180143886db84c9031798df188ae7c"
